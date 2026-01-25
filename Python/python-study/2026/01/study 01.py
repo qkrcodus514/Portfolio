95a60d8d2ece3d0b7e9a5eccff8e5b35c38885e9
parkcy <parkcy514@gmail.com>
2026-01-25 20:15:44 +0900

    Resolve conflicts and add Coding folder with Python files

--- Python/python-study/2026/01/study 01.py
+++ Python/python-study/2026/01/study 01.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #파이썬의 기본 자료형
 print(123)
 123
@@ -29,3 +30,31 @@ print(4/2)
 num = 3
 print(num+5)
 8
+=======
+ print(123)
+123
+>>> print('Faker')
+Faker
+>>> print(1,2,3)
+1 2 3
+>>> age=13
+>>> name='T1'
+>>> grade = ['A+', 'A0', 'A-']
+>>> print(name)
+T1
+>>> print(age)
+13
+>>> print(grade)
+['A+', 'A0', 'A-']
+>>> print(3+5)
+8
+>>> print(3-2)
+1
+>>> print(3*8)
+24
+>>> print(4/2)
+2.0
+>>> num = 3
+>>> print(num+5)
+8
+>>>>>>> de5791f (Study: add Python practice file)
