a32ffeb7aa24785ab983a1afaee8a79a434f1f35
qkrcodus514 <parkcy514@gmail.com>
2026-01-25 17:46:19 +0900

    Study: add Python practice file
    Study and practice how to use Git and VS code

--- Python/python-study/2026/01/study 01.py
+++ Python/python-study/2026/01/study 01.py
@@ -1,26 +1,31 @@
- print(123)
+#파이썬의 기본 자료형
+print(123)
 123
->>> print('Faker')
+print('Faker')
 Faker
->>> print(1,2,3)
+print(1,2,3)
 1 2 3
->>> age=13
->>> name='T1'
->>> grade = ['A+', 'A0', 'A-']
->>> print(name)
+
+#파이썬 변수
+age=13
+name='T1'
+grade = ['A+', 'A0', 'A-']
+print(name)
 T1
->>> print(age)
+print(age)
 13
->>> print(grade)
+print(grade)
 ['A+', 'A0', 'A-']
->>> print(3+5)
+
+#자료형 사칙연산
+print(3+5)
 8
->>> print(3-2)
+print(3-2)
 1
->>> print(3*8)
+print(3*8)
 24
->>> print(4/2)
+print(4/2)
 2.0
->>> num = 3
->>> print(num+5)
+num = 3
+print(num+5)
 8
