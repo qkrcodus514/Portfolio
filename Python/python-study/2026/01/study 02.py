8d72c3f5ad21f98f045c412c5c38fd5426462648
parkcy <parkcy514@gmail.com>
2026-01-26 14:54:59 +0900

    Study: Four arithmetic operations and strings via Python

---
+++ Python/python-study/2026/01/study 02.py
@@ -0,0 +1,40 @@
+#변수와 연산자를 사용해 거꾸로 연산
+num1=12
+num2=num1+10
+answer=num2/2
+print('The answer must be', answer, '!')
+The answer must be 11.0 !
+
+#숫자형의 몫과 나머지
+print(13/5)
+2.6
+print(13//5)
+2
+print(13%5)
+3
+
+#숫자형 제곱
+print(2**8)
+256
+print(2**10)
+1024
+
+#거꾸로 계산
+ans1=4%3
+ans2=2**4
+print(as1, ans2)
+
+print(ans1, ans2)
+1 16
+
+#문자열 더하고 곱하기
+print('안녕'+'하세요')
+안녕하세요
+print('T1 사랑해'*3)
+T1 사랑해T1 사랑해T1 사랑해
+str1='Love'+'Faker'
+str2='Doran'*2
+T1=(str1+str2)*2
+print(T1)
+LoveFakerDoranDoranLoveFakerDoranDoran
+>>>
