58eed0654fb7df822af2d1e3f179bf7c70480841
qkrcodus514 <parkcy514@gmail.com>
2026-01-27 21:07:50 +0900

    [Study] Python Basic: Four arithmetic operations and strings

--- Python/python-study/2026/01/study 02.py
+++ Python/python-study/2026/01/study 02.py
@@ -1,28 +1,28 @@
 #변수와 연산자를 이용한 연산 예제
-num1=12
-num2=num1+10
-answer=num2/2
+num1 = 12
+num2 = num1 + 10
+answer = num2 / 2
 print('The answer must be', answer, '!')
 
 #숫자형의 몫과 나머지 예제
-print(13/5)
-print(13//5)
-print(13%5)
+print('13 / 5 =', 13 / 5)
+print('13 // 5 =', 13 // 5)
+print('13 % 5 =', 13 % 5)
 
 #숫자형 제곱 예제
-print(2**8)
-print(2**10)
+print(2 ** 8)
+print(2 ** 10)
 
 #거꾸로 계산 예제
-ans1=4%3
-ans2=2**4
+ans1 = 4 % 3
+ans2 = 2 ** 4
 print(ans1, ans2)
 
 #문자열 더하고 곱하기 예제
-print('안녕'+'하세요')
-print('T1 사랑해'*3)
+print('안녕' + '하세요')
+print('T1 사랑해' * 3)
 
-str1='Love'+'Faker'
-str2='Doran'*2
-T1=(str1+str2)*2
+str1 = 'Love'+'Faker'
+str2 = 'Doran' * 2
+T1 = (str1 + str2) * 2
 print(T1)
