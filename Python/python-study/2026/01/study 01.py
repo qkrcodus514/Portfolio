df2e0f95fe36567a8d3558d2c3b86fa9091ccc4e
qkrcodus514 <parkcy514@gmail.com>
2026-01-27 21:00:33 +0900

    [Study] Python Basic: Basic practice and GitHub utilization practice

--- Python/python-study/2026/01/study 01.py
+++ Python/python-study/2026/01/study 01.py
@@ -1,31 +1,20 @@
-#파이썬의 기본 자료형
+#기본 자료형 예제
 print(123)
-123
 print('Faker')
-Faker
 print(1,2,3)
-1 2 3
 
-#파이썬 변수
+#변수 에제
 age=13
 name='T1'
 grade = ['A+', 'A0', 'A-']
 print(name)
-T1
 print(age)
-13
 print(grade)
-['A+', 'A0', 'A-']
 
-#자료형 사칙연산
+#자료형 사칙연산 예제
 print(3+5)
-8
 print(3-2)
-1
 print(3*8)
-24
 print(4/2)
-2.0
 num = 3
 print(num+5)
-8
