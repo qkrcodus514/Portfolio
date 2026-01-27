bb09790bb16410a5b63433bea037195587442f14
qkrcodus514 <parkcy514@gmail.com>
2026-01-27 21:02:28 +0900

    [Study] Python Basic: Four arithmetic operations and strings

--- Python/python-study/2026/01/study 02.py
+++ Python/python-study/2026/01/study 02.py
@@ -1,39 +1,28 @@
-#변수와 연산자를 사용해 거꾸로 연산
+#변수와 연산자를 이용한 연산 예제
 num1=12
 num2=num1+10
 answer=num2/2
 print('The answer must be', answer, '!')
-The answer must be 11.0 !
 
-#숫자형의 몫과 나머지
+#숫자형의 몫과 나머지 예제
 print(13/5)
-2.6
 print(13//5)
-2
 print(13%5)
-3
 
-#숫자형 제곱
+#숫자형 제곱 예제
 print(2**8)
-256
 print(2**10)
-1024
 
-#거꾸로 계산
+#거꾸로 계산 예제
 ans1=4%3
 ans2=2**4
-print(as1, ans2)
-
 print(ans1, ans2)
-1 16
 
-#문자열 더하고 곱하기
+#문자열 더하고 곱하기 예제
 print('안녕'+'하세요')
-안녕하세요
 print('T1 사랑해'*3)
-T1 사랑해T1 사랑해T1 사랑해
+
 str1='Love'+'Faker'
 str2='Doran'*2
 T1=(str1+str2)*2
 print(T1)
-LoveFakerDoranDoranLoveFakerDoranDoran
