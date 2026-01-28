7649e7ec2d72f231b2c58609579306dc3f27faf4
parkcy <parkcy514@gmail.com>
2026-01-28 21:13:04 +0900

    [Sutdy] Python Basic: indexing, slicing, input and type conversion

---
+++ Python/python-study/2026/01/study 03.py
@@ -0,0 +1,28 @@
+
+#문자령 인덱싱 예제
+word = 'world'
+print(word[1])
+
+
+#리스트 인덱싱 및 값 변경
+animal = ['dog', 'cat', 'watermelon']
+animal[2] = 'lion'
+print(animal)
+
+#리스트 슬라이싱 예제
+beta = [2, 4, 6, 8, 10, 12, 14]
+
+#슬라이싱은 종료 인덱스를 포함하지 않음
+print(beta[2:5])
+
+#input() 예제
+var = input()
+print('hey', var)
+
+#형 변환 예제: 문자열을 정수로 변환
+a = '345'
+print(type(a))
+
+b = int('345')
+print(type(b)
+
