38c5adb503d04609ced4028e6bea873ce030eb68
parkcy <parkcy514@gmail.com>
2026-01-31 21:48:58 +0900

    [Refactor] Improve readability of Python study code

--- Python/python-study/2026/01/study 01.py
+++ Python/python-study/2026/01/study 01.py
@@ -1,20 +1,21 @@
-#기본 자료형 예제
-print(123)
-print('Faker')
-print(1,2,3)
+# 기본 자료형 예제
+print('정수 출력:', 123)
+print('문자열 출력:', 'Faker')
+print('여러 값 출력', 1,2,3)
 
-#변수 에제
-age=13
-name='T1'
-grade = ['A+', 'A0', 'A-']
-print(name)
-print(age)
-print(grade)
+# 변수 에제
+age =1 3
+name ='T1'
+grades = ['A+', 'A0', 'A-']
 
-#자료형 사칙연산 예제
-print(3 + 5)
-print(3 - 2)
-print(3 * 8)
-print(4 / 2)
+print('이름:', name)
+print('나이:', age)
+print('성적 목록:', grade)
+
+# 자료형 사칙연산 예제
 num = 3
-print(num + 5)
+
+print('num + 5 =', num + 5)
+print('num - 2 =', num - 2)
+print('num * 8 =', num * 8)
+print('num / 3 =', num / 3)
