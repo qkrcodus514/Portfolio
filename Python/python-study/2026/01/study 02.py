7b8a236ef7ba945e24a4c530f173f9de0ef05e86
parkcy <parkcy514@gmail.com>
2026-01-31 21:50:53 +0900

    [Refactor] Clean up spacing and indentation

--- Python/python-study/2026/01/study 02.py
+++ Python/python-study/2026/01/study 02.py
@@ -1,28 +1,38 @@
-#변수와 연산자를 이용한 연산 예제
+# 변수와 연산자를 이용한 연산 예제
 num1 = 12
 num2 = num1 + 10
 answer = num2 / 2
-print('The answer must be', answer, '!')
 
-#숫자형의 몫과 나머지 예제
-print('13 / 5 =', 13 / 5)
-print('13 // 5 =', 13 // 5)
-print('13 % 5 =', 13 % 5)
+print('정답은', answer, '이다!')
 
-#숫자형 제곱 예제
-print(2 ** 8)
-print(2 ** 10)
+# 숫자형의 몫과 나머지 예제
+price = 13
+people = 5
 
-#거꾸로 계산 예제
-ans1 = 4 % 3
-ans2 = 2 ** 4
-print(ans1, ans2)
+print('1인당 금액은:', price // people)
+print('남은 금액은:', price % people)
 
-#문자열 더하고 곱하기 예제
+# 숫자형 제곱 예제
+print('2의 8제곱은:', 2 ** 8)
+print('2의 10 제곱은:', 2 ** 10)
+
+# 거꾸로 계산 예제
+remainder = 4 % 3
+power_result = 2 ** 4
+
+print('나머지는:', remainder)
+print('제곱 결과:', power_result)
+
+# 문자열 더하고 곱하기 예제
 print('안녕' + '하세요')
-print('T1 사랑해' * 3)
+
+team = 'T1'
+cheer = team + ' 사랑해 '
+
+print(cheer * 3)
 
 str1 = 'Love'+'Faker'
 str2 = 'Doran' * 2
-T1 = (str1 + str2) * 2
-print(T1)
+result = (str1 + str2) * 2
+
+print(result)
