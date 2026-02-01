881f298eca63fc887c595b5766bb1b224d66d3a8
parkcy <parkcy514@gmail.com>
2026-02-01 20:38:51 +0900

    [Refactor] Apply PEP8 style to study code

--- Python/python-study/2026/01/study 03.py
+++ Python/python-study/2026/01/study 03.py
@@ -1,28 +1,33 @@
 
-#문자령 인덱싱 예제
+# 문자열 인덱싱 예제
 word = 'world'
-print(word[1])
+
+print('문자열 인덱싱 결과:', word[1])
 
 
-#리스트 인덱싱 및 값 변경
+# 리스트 인덱싱 및 값 변경
 animal = ['dog', 'cat', 'watermelon']
 animal[2] = 'lion'
-print(animal)
 
-#리스트 슬라이싱 예제
+print('변경된 리스트:', animal)
+
+# 리스트 슬라이싱 예제
 beta = [2, 4, 6, 8, 10, 12, 14]
 
-#슬라이싱은 종료 인덱스를 포함하지 않음
-print(beta[2:5])
+# 슬라이싱은 종료 인덱스를 포함하지 않음
+print('슬라이싱 결과:', beta[2:5])
 
-#input() 예제
-var = input()
-print('hey', var)
+# input() 예제
+name = input('이름을 입력하세요: ')
 
-#형 변환 예제: 문자열을 정수로 변환
+print('안녕하세요', name)
+
+# 형 변환 예제: 문자열을 정수로 변환
 a = '345'
-print(type(a))
+
+print('a의 타입은:', type(a))
 
 b = int('345')
-print(type(b)
+
+print('b의 타입은:', type(b))
 
