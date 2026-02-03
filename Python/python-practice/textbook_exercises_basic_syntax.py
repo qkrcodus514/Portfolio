cad82c55aded1d7783b938e7d7234022b33f881a
parkcy <parkcy514@gmail.com>
2026-02-03 22:59:23 +0900

    [Practice] Add Python practice based on textbook

---
+++ Python/python-practice/textbook_exercises_basic_syntax.py
@@ -0,0 +1,43 @@
+
+>>> print('*')
+*
+>>>  print('**')
+  File "<python-input-32>", line 1
+    print('**')
+IndentationError: unexpected indent
+>>> print('**')
+**
+>>> print('***')
+***
+>>> print('****')
+****
+>>> print('******')
+******
+>>> c = 18
+>>> f = c * (9/5) + 32
+>>> print(f)
+64.4
+>>> num = int(input())
+507
+>>> if 1 <= num <= 9:
+...     print('한 자리 숫자입니다.')
+... elif 10 <= num <= 99:
+...     print('두 자리 숫자입니다.')
+... else:
+...     print('세 자리 숫자입니다.')
+...
+세 자리 숫자입니다.
+>>> score = int(input())
+98
+
+>>> if score ==0:
+...     print('F')
+... elif 0 < score < 77:
+...     print('B+')
+... elif 77 <= score < 88:
+...     print('A0')
+... else:
+...     print('A+')
+...
+A+
+>>>
