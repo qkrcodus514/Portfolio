3195b06d1cfe50c96d2ea80f4b62f79c77c2dbcb
parkcy <parkcy514@gmail.com>
2026-02-03 22:52:44 +0900

    [Refactor] rename Pythin study file

---
+++ Python/python-study/01_basic_types_and_operations.py
@@ -0,0 +1,35 @@
+"""
+📘 Python Study Examples
+
+- Core concept examples only
+- Detailed explanations are documented in README.md
+- Practice and experimental code is maintained separately
+"""
+
+#===============================
+# [Example 01] 기본 자료형 출력
+#===============================
+print("정수 출력:", 123)
+print("문자열 출력:", "Faker")
+print("여러 값 출력", 1, 2, 3)
+
+# ==============================
+# [Example 02] 변수 사용
+# ==============================
+age = 13
+name ="T1"
+grades = ["A+", "A0", "A-"]
+
+print("이름:", name)
+print("나이:", age)
+print("성적 목록:", grades)
+
+# ==============================
+# [Example 03] 정수 자료형 사칙연산
+# ==============================
+num = 3
+
+print("num + 5 =", num + 5)
+print("num - 2 =", num - 2)
+print("num * 8 =", num * 8)
+print("num / 3 =", num / 3)
